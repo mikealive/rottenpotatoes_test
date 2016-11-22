cf08dbf7bb99f009ecd5df889081b5f9b89cdbd2
Xianzhi Liu <smalltigerson@gmail.com>
2016-11-22 00:44:03 -0600

    add features

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -16,11 +16,11 @@ class MoviesController < ApplicationController
     end
     @all_ratings = Movie.all_ratings
     @selected_ratings = params[:ratings] || session[:ratings] || {}
-    
+
     if @selected_ratings == {}
       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
-    
+
     if params[:sort] != session[:sort]
       session[:sort] = sort
       flash.keep
@@ -64,4 +64,13 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def similar
+    @movie=Movie.find(params[:id])
+    director=@movie.Director
+    if(director.nil?)
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    end
+    @allsame=Movie.where(Director: director).reject { |r| r.title==@movie.title }
+  end
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -5,15 +5,13 @@ Rottenpotatoes::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  get '/movies/similar/:id'=> 'movies#similar', :as => 'moviesimilar'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
-
   # Sample resource route with options:
   #   resources :products do
   #     member do
